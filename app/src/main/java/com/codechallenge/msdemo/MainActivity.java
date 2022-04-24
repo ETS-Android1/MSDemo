7fc47ff829613fb98b00f51a45467f4168ad070b
Fuyao Zhang <lionsaberzfy@gmail.com>
2022-04-24 01:04:18 -0700

    LoginFragment related code clean up and step up string resources

--- app/src/main/java/com/codechallenge/msdemo/MainActivity.java
+++ app/src/main/java/com/codechallenge/msdemo/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.codechallenge.msdemo.fragment.LoginFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginFragment loginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_layout_container, loginFragment).commit();
     }
 }

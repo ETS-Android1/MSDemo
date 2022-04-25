7f73b9f2bc6a863fce187d33aee192890864ba4d
Fuyao Zhang <lionsaberzfy@gmail.com>
2022-04-24 18:01:17 -0700

    add LoginModel implemented input check related function

--- app/src/main/java/com/codechallenge/msdemo/model/LoginModel.java
+++ app/src/main/java/com/codechallenge/msdemo/model/LoginModel.java
@@ -1,4 +1,23 @@
 package com.codechallenge.msdemo.model;
 
+import android.util.Log;
+
+import com.codechallenge.msdemo.util.ErrorType;
+
 public class LoginModel {
+    private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    public LoginModel() {
+
+    }
+
+    public ErrorType evaluateInput(String emailAddress, int passwordLength) {
+        boolean emailAddressCheck = (emailAddress == null || emailAddress.length() == 0 || !emailAddress.matches(emailPattern)) ? false : true;
+        boolean passwordCheck = (passwordLength >= 8 && passwordLength <= 16) ? true : false;
+
+        if (!emailAddressCheck && !passwordCheck) return ErrorType.EMAIL_PASSWORD_ERROR;
+        if (!emailAddressCheck) return ErrorType.EMAIL_ERROR;
+        if (!passwordCheck) return ErrorType.PASSWORD_ERROR;
+        return ErrorType.PASSED;
+    }
 }
--- app/src/main/java/com/codechallenge/msdemo/present/LoginController.java
+++ app/src/main/java/com/codechallenge/msdemo/present/LoginController.java
@@ -1,13 +1,15 @@
 package com.codechallenge.msdemo.present;
 
+import com.codechallenge.msdemo.model.LoginModel;
 import com.codechallenge.msdemo.util.ErrorType;
 
 public class LoginController implements LoginPresent {
-
+    private LoginModel loginModel;
     public LoginController() {
+        loginModel = new LoginModel();
     }
     @Override
-    public ErrorType onSubmitted(String email, int PasswordLength) {
-        return ErrorType.PASSED;
+    public ErrorType onSubmitted(String emailAddress, int passwordLength) {
+        return loginModel.evaluateInput(emailAddress, passwordLength);
     }
 }

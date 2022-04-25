03c69294a4962fbbb5e879b4e14ec393d639ee82
Fuyao Zhang <lionsaberzfy@gmail.com>
2022-04-24 22:14:43 -0700

    add hideBottomUIMenu for MainActivity to control the fullscreen and style

--- app/src/main/java/com/codechallenge/msdemo/ux/MainActivity.java
+++ app/src/main/java/com/codechallenge/msdemo/ux/MainActivity.java
@@ -2,7 +2,9 @@ package com.codechallenge.msdemo.ux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.codechallenge.msdemo.R;
 import com.codechallenge.msdemo.ux.LoginFragment;
@@ -16,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
         // Use fragment to build application interactive View
         LoginFragment loginFragment = new LoginFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_layout_container, loginFragment).commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideBottomUIMenu();
+    }
+
+    /**
+     * Navigation bar hidden
+     */
+    protected void hideBottomUIMenu() {
+
+        if ( Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+
+        } else if (Build.VERSION.SDK_INT >= 19) {
+
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+
+        }
+
     }
 }

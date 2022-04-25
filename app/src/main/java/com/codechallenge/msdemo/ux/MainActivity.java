2bcdc81cb251620012ba3cd4a091bf2281faf542
Fuyao Zhang <lionsaberzfy@gmail.com>
2022-04-24 23:05:30 -0700

    add soft keyboard hide function

--- app/src/main/java/com/codechallenge/msdemo/ux/MainActivity.java
+++ app/src/main/java/com/codechallenge/msdemo/ux/MainActivity.java
@@ -2,19 +2,24 @@ package com.codechallenge.msdemo.ux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.codechallenge.msdemo.R;
 import com.codechallenge.msdemo.ux.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private InputMethodManager inputMethodManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         // Use fragment to build application interactive View
         LoginFragment loginFragment = new LoginFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_layout_container, loginFragment).commit();
@@ -27,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * If soft-keyboard board present, hide soft-keyboard when user clicks out of keyboard range.
+     * @param event user touch event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // TODO Auto-generated method stub
+        if (event.getAction() == MotionEvent.ACTION_DOWN && this.getCurrentFocus() != null && this.getCurrentFocus().getWindowToken() != null) {
+            inputMethodManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        return super.onTouchEvent(event);
+    }
+
+    /**
      * Navigation bar hidden
      */
     protected void hideBottomUIMenu() {
 
-        if ( Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
 
             View v = this.getWindow().getDecorView();
             v.setSystemUiVisibility(View.GONE);
